32dea7925ac6abc081d95265e4d8b0e83a79ec28
DESKTOP-M5UF1IQ\orlov <orlovaad@gmail.com>
2023-01-15 10:15:02 +0400

    add filter and sort logic, make button click

--- src/App.js
+++ src/App.js
@@ -1,33 +1,91 @@
 import logo from './assets/logo.svg';
-import FilterTransfers from './components/FilterTransfers';
+import FilterTransfers from './components/FilterTransfers/FilterTransfers';
 import FilterSuitable from './components/FilterSuitable';
 import './App.css';
-import searchIdStore from './responseSearchId.json';
 import ticketsStore from './responseTickets.json';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Ticket from './components/Ticket';
+import { CHEAP_FILTER_TYPE, FAST_FILTER_TYPE, OPTIMAL_FILTER_TYPE } from './components/FilterSuitable/consts';
+// import {
+//   ALL_TRANSFERS_COUNT,
+//   NULL_TRANSFERS_COUNT,
+//   ONE_TRANSFERS_COUNT,
+//   TWO_TRANSFERS_COUNT,
+//   THREE_TRANSFERS_COUNT,
+// } from './components/FilterTransfers/constsTransfers';
+import { TRANSFER_LABELS, TRANSFER_LABELS_INDEX } from './components/FilterTransfers/constsTransfers';
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
 function App() {
-  const [searchID, setSearchId] = useState();
-  const [tickets, setTickets] = useState([]);
-  const [sortTickets, setSortTickets] = useState([]);
-  const [stop, setStop] = useState(false);
+  const [filterSuitable, setFilterSuitable] = useState(CHEAP_FILTER_TYPE);
+  const [filterTransfers, setFilterTransfers] = useState(new Array(Object.keys(TRANSFER_LABELS).length).fill(false));
+  const [maxCountTickets, setMaxCountTickets] = useState(5);
 
-  useEffect(() => {
-    setSearchId(searchIdStore.searchId);
+  const sortPrice = (sortedTickets) => {
+    return sortedTickets.sort((a, b) => a.price - b.price);
+  };
+  const sortDuration = (sortedTickets) => {
+    return sortedTickets.sort(
+      (a, b) => a.segments[0].duration + a.segments[1].duration - (b.segments[0].duration + b.segments[1].duration)
+    );
+  };
+  const sortOptimal = (sortedTickets) => {
+    return sortedTickets.sort(
+      (a, b) =>
+        a.price - b.price ||
+        a.segments[0].duration + a.segments[1].duration - (b.segments[0].duration + b.segments[1].duration)
+    );
+  };
 
-    if (searchID && stop === false) {
-      function subscribe() {
-        setTickets([...tickets, ...ticketsStore.tickets]);
-        if (ticketsStore.stop) {
-          setStop(ticketsStore.stop);
-        }
+  const getTransfersCount = () => {
+    let indexTransfers = [];
+    filterTransfers.map((elem, index) => {
+      if (elem === true) {
+        indexTransfers.push(index - 1);
       }
-      subscribe();
-    }
 
-    setSortTickets(tickets.slice(0, 5));
-  }, [searchID, tickets, stop]);
+      return elem;
+    });
+    return indexTransfers;
+  };
+
+  const sortTransfers = () => {
+    const transfersCount = getTransfersCount();
+    console.log(transfersCount);
+
+    if (transfersCount[0] === -1 || transfersCount.length === 0) {
+      return [...ticketsStore.tickets].slice(0, maxCountTickets);
+    }
+    console.log(maxCountTickets);
+    return [...ticketsStore.tickets]
+      .filter((ticket, index) => {
+        let isChecked = false;
+        for (let i = 0; i < transfersCount.length; i++) {
+          if (
+            transfersCount[i] === ticket.segments[0].stops.length ||
+            transfersCount[i] === ticket.segments[1].stops.length
+          ) {
+            isChecked = true;
+          }
+        }
+        console.log(ticket, index, isChecked);
+        return isChecked;
+      })
+
+      .slice(0, maxCountTickets);
+  };
+
+  const printTickets = () => {
+    console.log(sortTransfers());
+    if (filterSuitable === CHEAP_FILTER_TYPE) {
+      return sortPrice(sortTransfers()).map((ticket) => <Ticket {...ticket} />);
+    } else if (filterSuitable === FAST_FILTER_TYPE) {
+      return sortDuration(sortTransfers()).map((ticket) => <Ticket {...ticket} />);
+    } else {
+      return sortOptimal(sortTransfers()).map((ticket) => <Ticket {...ticket} />);
+    }
+  };
+
   return (
     <div className="App">
       <div className="appWrapper">
@@ -35,14 +93,17 @@ function App() {
           <img src={logo} alt="logo" />
         </div>
         <div className="main">
-          <FilterTransfers />
-          <FilterSuitable />
+          <FilterTransfers setFilterTransfers={setFilterTransfers} filterTransfers={filterTransfers} />
+          <FilterSuitable setFilterSuitable={setFilterSuitable} filterSuitable={filterSuitable} />
           <div className="tickets">
-            {sortTickets.map((ticket) => (
-              <Ticket {...ticket} />
-            ))}
-
-            <div className="moreTickets">Показать еще 5 билетов!</div>
+            {printTickets()}
+            <button
+              className="moreTickets"
+              style={{ visibility: maxCountTickets < ticketsStore.tickets.length ? 'visible' : 'hidden' }}
+              onClick={() => setMaxCountTickets(maxCountTickets + 5)}
+            >
+              Показать еще 5 билетов!
+            </button>
           </div>
         </div>
       </div>
---
+++ src/components/FilterSuitable/consts.js
@@ -0,0 +1,3 @@
+export const CHEAP_FILTER_TYPE = 'cheap';
+export const FAST_FILTER_TYPE = 'fast';
+export const OPTIMAL_FILTER_TYPE = 'optimal';
